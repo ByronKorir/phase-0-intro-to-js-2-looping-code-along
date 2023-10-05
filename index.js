345a106a7d0ead404fb8819f4292c8d715175bab
byron korir <byron.korir@student.moringaschool.com>
2023-10-05 12:01:18 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names) {
+   const thankYouMessages = [];
+   for (let i = 0; i < names.length; i++) {
+     thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+   }
+   return thankYouMessages;
+ }
+
+ function countDown(){
+   let j= 10
+   while(j>=0){
+      console.log(j);
+      j--;
+   }
+   
+ }
